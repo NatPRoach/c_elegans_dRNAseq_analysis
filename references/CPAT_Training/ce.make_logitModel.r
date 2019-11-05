0c9b8edaa5ab5f9c3809be60c35a16f2447bde72
Nathan Roach <nroach2@jhu.edu>
2019-11-05 12:19:28 -0500

    Fixing problem with CPAT RScript

--- references/CPAT_Training/ce.make_logitModel.r
+++
@@ -1,4 +0,0 @@
-data <- read.table(file="ce.feature.xls",sep="\t",header=T)
-attach(data)
-mylogit <- glm(Label ~ mRNA + ORF + Fickett + Hexamer, family=binomial(link="logit"), na.action=na.pass)
-save.image("ce.logit.RData")
